b5b516f3dc00190e066cc6b97685aa9eed95f5ce
saeel123 <saeel@dccper.com>
2017-05-24 15:09:24 +0530

    Added todos routes

--- server.js
+++ server.js
@@ -2,8 +2,51 @@ var express = require('express');
 var app = express();
 var PORT =  process.env.PORT || 3000;
 
+var todos = [{
+  id: 1,
+  description: 'fix bike',
+  completed: false
+},{
+  id: 2,
+  description: 'fix Pc',
+  completed: false
+},{
+  id: 3,
+  description: 'fix',
+  completed: true
+}]
+
 app.get('/', function (req, res) {
   res.send('ToDo app test');
+});
+
+app.get('/todos', function (req, res) {
+  res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+  var id = parseInt(req.params.id, 10);
+  var matchTodo;
+
+  console.log(id);
+
+  if (typeof id === 'number') {
+    for (i = 0; i < todos.length; i++) {
+      console.log(todos[i].id);
+      if (todos[i].id === id) {
+        matchTodo = todos[i];
+      }
+    }
+  } else {
+    res.json("please enter number id")
+  }
+
+  if (matchTodo) {
+    res.json(matchTodo);
+  } else {
+    res.status(404).send();
+  }
+
 });
 
 app.listen(PORT, function (req, res) {
