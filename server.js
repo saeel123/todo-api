cd9407d1cd1a004db1d5c172b1e4ae305e9786ac
saeel123 <saeel@dccper.com>
2017-05-25 11:51:25 +0530

    Added post route

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var PORT =  process.env.PORT || 3000;
 
-var todos = [{
-  id: 1,
-  description: 'fix bike',
-  completed: false
-},{
-  id: 2,
-  description: 'fix Pc',
-  completed: false
-},{
-  id: 3,
-  description: 'fix',
-  completed: true
-}]
+app.use(bodyParser.json());
+
+var todos = [];
+var todoNextId = 1;
 
 app.get('/', function (req, res) {
   res.send('ToDo app test');
@@ -27,8 +19,6 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res) {
   var id = parseInt(req.params.id, 10);
   var matchTodo;
-
-  console.log(id);
 
   if (typeof id === 'number') {
     for (i = 0; i < todos.length; i++) {
@@ -46,6 +36,18 @@ app.get('/todos/:id', function (req, res) {
   } else {
     res.status(404).send();
   }
+
+});
+
+
+app.post('/todos', function (req, res) {
+  var body = req.body;
+  body.id = todoNextId ++;
+
+
+  todos.push(body);
+  res.json(body);
+
 
 });
 
